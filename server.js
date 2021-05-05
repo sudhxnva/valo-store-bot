3c24bca2bda5fc315b50fdadfc1aa9beed21b892
Sudhanva <sudhanva.m@pm.me>
2021-05-05 11:07:40 +0530

    Add error handling

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ client.on("ready", async () => {
 });
 
 client.on("message", async (message) => {
-  try {
-    if (!message.content.startsWith("!") || message.author.bot) return;
-    if (message.content === "!store") {
+  if (!message.content.startsWith("!") || message.author.bot) return;
+
+  if (message.content === "!store") {
+    try {
       if (message.channel.type === "dm")
         return message.reply("Use this command on the server!");
       const user = await User.findOne({ discordID: message.author.id });
@@ -29,54 +30,63 @@ client.on("message", async (message) => {
         return message.author.send({ embed: generateRegisterEmbed() });
       }
       const waitMessage = await message.channel.send("Fetching, hol up...");
-      const valorant = getClient(user.riotUsername, decrypt(user.riotPassword));
-      const skins = await getSkins(valorant);
-      const embed = generateSkinsEmbed(valorant.user.GameName, skins);
-      waitMessage.delete();
-      message.channel.send({ embed });
-    }
-
-    if (message.content.startsWith("!register")) {
-      if (message.channel.type !== "dm") {
-        message.delete({ reason: "Send personal details only on DM" });
-        return message.reply(
-          "Hey! Don't share your credentials on the server. Send it on DM only!"
+      try {
+        const valorant = getClient(
+          user.riotUsername,
+          decrypt(user.riotPassword)
         );
+        const skins = await getSkins(valorant);
+        const embed = generateSkinsEmbed(valorant.user.GameName, skins);
+        waitMessage.delete();
+        message.channel.send({ embed });
+      } catch (err) {
+        waitMessage.delete();
+        console.error(err);
+        message.channel.send("Sorry! I have trouble connecting to the store");
       }
-      const args = message.content.slice("!register".length).trim().split(" ");
-      if (args.length != 2) return message.reply("Invalid format!");
-
-      const waitMessage = await message.reply("Authenticating...");
-
-      const details = {
-        discordName: message.author.username,
-        riotUsername: args[0],
-        riotPassword: encrypt(args[1]),
-      };
-      const valorant = getClient(args[0], args[1]);
-
-      valorant
-        .login()
-        .then(async (res) => {
-          await User.findOneAndUpdate(
-            { discordID: message.author.id },
-            details,
-            { upsert: true, new: true }
-          );
-          waitMessage.delete();
-          message.reply(
-            `Success! Welcome aboard, **${res.user.GameName}**!\nUse the command` +
-              "`!store` on the server to view the guns on sale on your store!"
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-          waitMessage.delete();
-          message.reply("Sorry, Invalid credentials! Please try again");
-        });
+    } catch (error) {
+      message.channel.send("Sorry, I ran into an error.");
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
+  }
+
+  if (message.content.startsWith("!register")) {
+    if (message.channel.type !== "dm") {
+      message.delete({ reason: "Send personal details only on DM" });
+      return message.reply(
+        "Hey! Don't share your credentials on the server. Send it on DM only!"
+      );
+    }
+    const args = message.content.slice("!register".length).trim().split(" ");
+    if (args.length != 2) return message.reply("Invalid format!");
+
+    const waitMessage = await message.reply("Authenticating...");
+
+    const details = {
+      discordName: message.author.username,
+      riotUsername: args[0],
+      riotPassword: encrypt(args[1]),
+    };
+    const valorant = getClient(args[0], args[1]);
+
+    valorant
+      .login()
+      .then(async (res) => {
+        await User.findOneAndUpdate({ discordID: message.author.id }, details, {
+          upsert: true,
+          new: true,
+        });
+        waitMessage.delete();
+        message.reply(
+          `Success! Welcome aboard, **${res.user.GameName}**!\nUse the command` +
+            "`!store` on the server to view the guns on sale on your store!"
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+        waitMessage.delete();
+        message.reply("Sorry, Invalid credentials! Please try again");
+      });
   }
 });
 
