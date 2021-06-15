1087d26cbc6b34fb6ba8fe8f10e6e87efb977d42
Sudhanva <sudhanva.m@pm.me>
2021-06-15 20:32:52 +0530

    Added more logs

--- util/valo.js
+++ util/valo.js
@@ -12,13 +12,16 @@ function getClient(username, password, shard = "AP") {
 async function getSkins(valorant) {
   try {
     const user = await valorant.login();
+    console.log("Val: Logged in");
     const { skins } = await valorant.storeApi.getStorefront(
       valorant.user.Subject,
       true
     );
+    console.log("Val: Skins retrieved");
     const { Identity } = await valorant.playerApi.getInventory(
       user.user.Subject
     );
+    console.log("Val: Playercard retrieved");
     return {
       skins,
       Identity,
