689d5d7b6404b1e85744d27462723f1df0d6af69
Kaustubh Eppalapalli <kaustubh.e@gmail.com>
2021-08-13 03:01:21 +0530

    added createCipherKey function to tackle invalid length issue

--- util/crypto.js
+++ util/crypto.js
@@ -2,14 +2,18 @@ const crypto = require("crypto");
 require("dotenv").config();
 
 const algorithm = "aes-256-ctr";
-const secretKey = process.env.SECRET;
+const secretKey = Buffer.from(process.env.SECRET);
 const iv = crypto.randomBytes(16);
 
+function createCipherKey (secret) {
+  return crypto.createHash('md5').update(secret).digest('hex')
+}
+
 const encrypt = (text) => {
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+  const cipherKey = createCipherKey(secretKey)
+  const cipher = crypto.createCipheriv(algorithm, cipherKey, iv);
 
   const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-
   return {
     iv: iv.toString("hex"),
     content: encrypted.toString("hex"),
@@ -17,9 +21,10 @@ const encrypt = (text) => {
 };
 
 const decrypt = (hash) => {
+  const cipherKey = createCipherKey(secretKey)
   const decipher = crypto.createDecipheriv(
     algorithm,
-    secretKey,
+    cipherKey,
     Buffer.from(hash.iv, "hex")
   );
 
