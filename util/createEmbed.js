d30cb0c692d453e95b0e121e07e80782a6f874ab
FATE <survfate@gmail.com>
2021-12-16 12:38:33 -0800

    Make function for html template generation

--- util/createEmbed.js
+++ util/createEmbed.js
@@ -23,7 +23,7 @@ async function generateSkinsEmbedMessage(
   rank = "Unknown",
   message
 ) {
-  let images = "";
+  let imagesDiv = "";
   let priceTier = "";
 
   for (const skin of skins) {
@@ -46,169 +46,10 @@ async function generateSkinsEmbedMessage(
             class="skin-image ${skinType(skin.name, skin.cost.amount)}" />
       </div>
     </div>`;
-    images += imageDiv;
+    imagesDiv += imageDiv;
   }
 
-  const template = `
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ValoStoreBot</title>
-    <style>
-    :root{
-            --body-width: 740px;
-            --body-height: 400px;
-            --grid-width: 360px;
-            --skin-bg-height: 170px;
-            --skin-bg-width: 340px; /* Skin bg ratio 2:1 (width: height) */
-            --text-margin: 8px;
-            --text-price-margin: 10px;
-        }
-
-        * {
-            /* margin: 0; */
-            padding: 0;
-            box-sizing: border-box;
-            font-family: sans-serif;
-        }
-
-        body{
-            width: var(--body-width);
-            height: var(--body-height);
-        }
-
-        .skins-container {
-            width: var(--body-width);
-            height: var(--body-height);
-            padding: 9px 0;
-            background-color: rgb(0, 0, 0);
-            display: grid;
-            grid-template-columns: var(--grid-width) var(--grid-width);
-            justify-content: center;
-        }
-
-        .skin-wrapper {
-            color: #fff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            min-width: fit-content;
-            padding: 0px;
-            overflow: hidden;
-        }
-
-        .skin-bg {
-            background-color: rgb(29 29 29);
-            overflow: hidden;
-            height: var(--skin-bg-height);
-            border-radius: 5px;
-            position: relative;
-            width: var(--skin-bg-width);
-            max-width: var(--skin-bg-width);
-        }
-
-        .skin-image {
-            max-height: 125px;
-            margin-top: 50%;
-            margin-left: 50%;
-            transform: translate(-54%, -116%) rotate(27deg);
-            z-index: 200;
-        }
-
-        .skin-image.knife {
-            transform: translate(-51%, -126%) rotate(45deg);
-        }
-
-        .skin-image.frenzy {
-          transform: translate(-54%, -116%) rotate(27deg) scale(1.1);
-        }
-
-        .skin-image.shorty,
-        .skin-image.ghost {
-          transform: translate(-54%, -116%) rotate(27deg) scale(0.7);
-        },
-
-        .skin-image.bulldog {
-          transform: translate(-54%, -116%) rotate(27deg) scale(0.8);
-        }
-
-        .skin-image.marshal {
-          transform: translate(-50%, -120%) rotate(27deg) scale(0.9);
-        }
-
-        .skin-text {
-            font-weight: 700;
-            font-style: italic;
-            color: rgb(231, 229, 229);
-            font-size: 16px;
-            text-transform: uppercase;
-        }
-
-        .skin-text.skin-title.bg{
-            position: absolute;
-            padding: 2px;
-            font-size: 75px;
-            color: rgb(255 255 255 / 5%);
-        }
-
-        .skin-text.skin-title{
-            position: absolute;
-            left: 5px;
-            bottom: 5px;
-            padding: 2px;
-            border-radius: 5px;
-            max-width: 150px;
-            font-size: 20px;
-            color: rgba(255, 255, 255, 0.911);
-        }
-
-        .skin-text.skin-price {
-            position: absolute;
-            right: var(--text-price-margin);
-            top: var(--text-price-margin);
-            padding-right: 4px;
-            font-weight: 100;
-            font-style: normal;
-            display: inline-flex;
-            background-color: rgb(0 0 0 / 36%);
-            padding: 4px;
-            border-radius: 2px;
-            z-index: 1;
-        }
-
-        .skin-price-text{
-            padding-left: 7px; 
-            margin-top: auto; 
-            margin-bottom: auto; 
-            font-size: 16px;
-            opacity: 0.85;
-        }
-
-        .price-tier-img {
-            display: grid;
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-        }
-
-        .content-tier{
-            align-self: center;
-            width: 20px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="skins-container">
-        ${images}
-    </div>
-</body>
-
-</html>`;
+  const template = skinsHtmlTemplate(imagesDiv, 4);
 
   const image = await nodeHtmlToImage({
     html: template,
@@ -255,7 +96,7 @@ async function generateMarketEmbedMessage(
   },
   message
 ) {
-  let images = "";
+  let imagesDiv = "";
   let priceTier = "";
 
   for (const skin of bonusSkins) {
@@ -283,9 +124,81 @@ async function generateMarketEmbedMessage(
             class="skin-image ${skinType(skin.offer.rewards[0].name, skinBaseCost)}" />
       </div>
     </div>`;
-    images += imageDiv;
+    imagesDiv += imageDiv;
   }
 
+  const template = skinsHtmlTemplate(imagesDiv, 6);
+
+  const image = await nodeHtmlToImage({
+    html: template,
+    puppeteerArgs: {
+      args: ["--no-sandbox"],
+    },
+    encoding: "buffer",
+    transparent: true,
+  });
+
+  const attachment = new MessageAttachment(image, "image.png");
+  const embed = new MessageEmbed()
+    .setTitle(`${message.author.username}'s Valorant Night Market`)
+    .setDescription(
+      `> **Gamer Tag:** ${user.name}#${user.tag} 
+      > **Valorant Points:** ${balance.find(x => x.name === 'Valorant Points').amount}
+      > **Radiant Points:** ${balance.find(x => x.name === 'Radiant Points').amount} 
+      > **Region:** ${user.region.toUpperCase()}
+      Here are the offers in your Night Market:
+      ${AccountLevel === 0 ? "" : `> **Account Level:** ${AccountLevel}`}`
+    )
+    .setThumbnail(
+      `https://media.valorant-api.com/playercards/${PlayerCardID}/smallart.png`
+    )
+    .setImage("attachment://image.png")
+    .setTimestamp(new Date())
+    .setFooter(process.env.EMBED_FOOTER || "Bot by VIPΞR#4643");
+
+  return { 
+    embeds: [embed],
+    files: [attachment],
+    allowedMentions: {
+      repliedUser: false
+    }
+  };
+}
+
+function skinPriceTier(baseprice) {
+  switch (baseprice) {
+    case 875:
+      return "12683d76-48d7-84a3-4e09-6985794f0445";
+    case 1275:
+      return "0cebb8be-46d7-c12a-d306-e9907bfc5a25";
+    case 1775:
+      return "60bca009-4182-7998-dee7-b8a2558dc369";
+    case 2175:
+      return "60bca009-4182-7998-dee7-b8a2558dc369";
+    case 2475:
+      return "411e4a55-4e59-7757-41f0-86a53f101bb5";
+    default:
+      return "e046854e-406c-37f4-6607-19a9ba8426fc";
+  }
+}
+
+function skinType(name, baseprice){
+  if (isKnife(name,baseprice))
+    return "knife";
+  if (name.includes('Shorty'))
+    return "shorty";
+  if (name.includes('Frenzy'))
+    return "frenzy";
+  if (name.includes('Ghost'))
+    return "ghost";
+  if (name.includes('Bulldog'))
+    return "bulldog";
+  if (name.includes('Marshal'))
+    return "marshal";
+  return "";
+}
+
+function skinsHtmlTemplate(imagesDiv, skinCount){
   const template = `
   <!DOCTYPE html>
   <html lang="en">
@@ -298,7 +211,7 @@ async function generateMarketEmbedMessage(
     <style>
     :root{
             --body-width: 740px;
-            --body-height: 600px;
+            --body-height: ${Math.round(skinCount/2)*200}px;
             --grid-width: 360px;
             --skin-bg-height: 170px;
             --skin-bg-width: 340px; /* Skin bg ratio 2:1 (width: height) */
@@ -311,11 +224,6 @@ async function generateMarketEmbedMessage(
             padding: 0;
             box-sizing: border-box;
             font-family: sans-serif;
-        }
-
-        body{
-            width: var(--body-width);
-            height: var(--body-height);
         }
 
         .skins-container {
@@ -472,79 +380,13 @@ async function generateMarketEmbedMessage(
 
 <body>
     <div class="skins-container">
-        ${images}
+        ${imagesDiv}
     </div>
 </body>
 
 </html>`;
 
-  const image = await nodeHtmlToImage({
-    html: template,
-    puppeteerArgs: {
-      args: ["--no-sandbox"],
-    },
-    encoding: "buffer",
-    transparent: true,
-  });
-
-  const attachment = new MessageAttachment(image, "image.png");
-  const embed = new MessageEmbed()
-    .setTitle(`${message.author.username}'s Valorant Night Market`)
-    .setDescription(
-      `> **Gamer Tag:** ${user.name}#${user.tag} 
-      > **Valorant Points:** ${balance.find(x => x.name === 'Valorant Points').amount}
-      > **Radiant Points:** ${balance.find(x => x.name === 'Radiant Points').amount} 
-      > **Region:** ${user.region.toUpperCase()}
-      Here are the offers in your Night Market:
-      ${AccountLevel === 0 ? "" : `> **Account Level:** ${AccountLevel}`}`
-    )
-    .setThumbnail(
-      `https://media.valorant-api.com/playercards/${PlayerCardID}/smallart.png`
-    )
-    .setImage("attachment://image.png")
-    .setTimestamp(new Date())
-    .setFooter(process.env.EMBED_FOOTER || "Bot by VIPΞR#4643");
-
-  return { 
-    embeds: [embed],
-    files: [attachment],
-    allowedMentions: {
-      repliedUser: false
-    }
-  };
-}
-
-function skinPriceTier(baseprice) {
-  switch (baseprice) {
-    case 875:
-      return "12683d76-48d7-84a3-4e09-6985794f0445";
-    case 1275:
-      return "0cebb8be-46d7-c12a-d306-e9907bfc5a25";
-    case 1775:
-      return "60bca009-4182-7998-dee7-b8a2558dc369";
-    case 2175:
-      return "60bca009-4182-7998-dee7-b8a2558dc369";
-    case 2475:
-      return "411e4a55-4e59-7757-41f0-86a53f101bb5";
-    default:
-      return "e046854e-406c-37f4-6607-19a9ba8426fc";
-  }
-}
-
-function skinType(name, baseprice){
-  if (isKnife(name,baseprice))
-    return "knife";
-  if (name.includes('Shorty'))
-    return "shorty";
-  if (name.includes('Frenzy'))
-    return "frenzy";
-  if (name.includes('Ghost'))
-    return "ghost";
-  if (name.includes('Bulldog'))
-    return "bulldog";
-  if (name.includes('Marshal'))
-    return "marshal";
-  return "";
+return template;
 }
 
 function isKnife(name, baseprice) {
